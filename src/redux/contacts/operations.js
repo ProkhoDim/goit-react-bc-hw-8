fa9116592cb5ef1adf33071cdd13e274a4bc17e0
Dmitij Prokhorenko <dmitrij.prokhorenko@gmail.com>
2022-12-02 17:55:14 +0200

    Added progress info and error showing to contacts actions

--- src/redux/contacts/operations.js
+++ src/redux/contacts/operations.js
@@ -1,14 +1,23 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
+import { toast } from 'react-hot-toast';
 
 export const fetchContacts = createAsyncThunk(
   'contacts/getAllContacts',
   async (_, thunkApi) => {
     try {
-      const response = await axios.get('/contacts');
+      const response = await toast.promise(
+        axios.get('/contacts'),
+        {
+          loading: 'Retrieving your contacts',
+          success: null,
+          error: 'Something went wrong',
+        },
+        { success: { style: { display: 'none' } } }
+      );
       return response.data;
     } catch (error) {
-      return thunkApi.rejectWithValue(error.message);
+      return;
     }
   }
 );
@@ -17,10 +26,17 @@ export const addContact = createAsyncThunk(
   'contacts/addNew',
   async ({ name = '', number = '' }, thunkApi) => {
     try {
-      const response = await axios.post('/contacts', { name, number });
+      const response = await toast.promise(
+        axios.post('/contacts', { name, number }),
+        {
+          loading: 'Adding new contact...',
+          success: 'Successfuly added!',
+          error: 'Something went wrong...',
+        }
+      );
       return response.data;
     } catch (error) {
-      return thunkApi.rejectWithValue(error.message);
+      return error;
     }
   }
 );
@@ -29,10 +45,17 @@ export const editContact = createAsyncThunk(
   'contacts/editContact',
   async ({ id = 0, ...restOptions }, thunkApi) => {
     try {
-      const response = await axios.patch(`/contacts/${id}`, restOptions);
+      const response = await toast.promise(
+        axios.patch(`/contacts/${id}`, restOptions),
+        {
+          loading: 'Editing contact...',
+          success: 'Contact was edited!',
+          error: 'Something went wrong...',
+        }
+      );
       return response.data;
     } catch (error) {
-      return thunkApi.rejectWithValue(error.message);
+      return error;
     }
   }
 );
@@ -41,10 +64,14 @@ export const removeContact = createAsyncThunk(
   'contacts/removeContact',
   async (id = 0, thunkApi) => {
     try {
-      const response = await axios.delete(`/contacts/${id}`);
+      const response = await toast.promise(axios.delete(`/contacts/${id}`), {
+        loading: 'Deleting contact...',
+        success: 'Contact was deleted!',
+        error: 'Something went wrong...',
+      });
       return response.data;
     } catch (error) {
-      return thunkApi.rejectWithValue(error.message);
+      return error;
     }
   }
 );
